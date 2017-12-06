88487f8c697e9f6c8c0d256626180612e4c17b71
christikaes <christikaes@gmail.com>
2017-12-06 12:56:40 -0500

    Part2-Add-MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
@@ -56,22 +57,24 @@ class App extends Component {
     ]
 
     return (
-      <div>
-        <h1>Robo 4 Hire</h1>
-        <span>Find a robot for every job!</span>
-        <div id="robots">
-          {robots.map(robot =>
-            <div className="robot">
-              <h3>{robot.name}</h3>
-              <img src={"http://www.robohash.org/" + robot.name} />
-              {robot.jobs.map(job =>
-                <div>{job}</div>
-              )}
-              <button>Hire Me!</button>
-            </div>
-          )}
-        </div>
-      </div >
+      <MuiThemeProvider>
+        <div>
+          <h1>Robo 4 Hire</h1>
+          <span>Find a robot for every job!</span>
+          <div id="robots">
+            {robots.map(robot =>
+              <div className="robot">
+                <h3>{robot.name}</h3>
+                <img src={"http://www.robohash.org/" + robot.name} />
+                {robot.jobs.map(job =>
+                  <div>{job}</div>
+                )}
+                <button>Hire Me!</button>
+              </div>
+            )}
+          </div>
+        </div >
+      </MuiThemeProvider>
     );
   }
 }
